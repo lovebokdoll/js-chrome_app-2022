df2a2d9e6f8f6af639f3b21feefab0225c0d494d
정다희 <dahee@jeongdahuiui-MacBookAir.local>
2022-11-01 12:15:05 +0900

    create clock

--- JS/clock.js
+++ JS/clock.js
@@ -1,10 +1,18 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("Hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 //2초마다 실핼시키고 싶음
 
-setInterval(sayHello, 5000);
+getClock();
+//get clock 즉시! --> 1초의 딜레이가 사라짐
+
+setInterval(getClock, 1000);
 //첫번째 argument: 실행하고자 하는 function
 //두번째 argument: 호출되는 function 간격을 몇 ms 로 할 지 쓴다
+
+//setTimeout(sayHello, 5000);
+//첫번째 argument: 실행하고자 하는 function
+//두번째 argument: 얼마나 기다릴지 ms 로 쓴다
